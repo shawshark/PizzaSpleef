934258c162efae09a70c7594223e16d9978c29ef
Chris <chrissbolding@gmail.com>
2014-04-03 20:07:56 +1100

    Create ArenaLocations.java

---
+++ src/me/shawshark/pizzaspleef/ArenaLocations.java
@@ -0,0 +1,16 @@
+package me.shawshark.pizzaspleef;
+
+import org.bukkit.Location;
+
+public class ArenaLocations {
+	
+	int arenaID;
+	int id;
+	Location location;
+	
+	public ArenaLocations(int arenaID, int id, Location location) {
+		this.arenaID = arenaID;
+		this.id = id;
+		this.location = location;
+	}
+}
