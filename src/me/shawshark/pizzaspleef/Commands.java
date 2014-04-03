18aea5448e5066ca28e55b310a7d770a2aad9e78
shawshark <chrissbolding@gmail.com>
2014-04-03 20:31:41 +1100

    Cleaned up code.
    - Added commands use.
    - blocks commands like /plugins, /pl etc..

--- src/me/shawshark/pizzaspleef/Commands.java
+++ src/me/shawshark/pizzaspleef/Commands.java
@@ -1,12 +1,30 @@
 package me.shawshark.pizzaspleef;
 
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
 public class Commands implements Listener {
 	
-	main m;
+	public main m;
 	
 	public Commands(main m) {
 		this.m = m;
+	}
+	
+	@EventHandler(priority=EventPriority.HIGHEST)
+	public void onPlayerCommand(PlayerCommandPreprocessEvent event) {
+		Player player = event.getPlayer();
+		String[] command = event.getMessage();
+		
+		if(command[0].equalsIgnoreCase("/plugins") || (command[0].equalsIgnoreCase("/pl") || (command[0].equalsIgnoreCase("/?")))) {
+			if(!player.hasPermission("plugins.viewbypass")) {
+				event.setCancelled(true);
+				player.sendMessage(m.Prefix + " " + ChatColor.RED + "You aren't allowed to view the plugins!");
+			}
+		}
 	}
 }
--- src/me/shawshark/pizzaspleef/main.java
+++ src/me/shawshark/pizzaspleef/main.java
@@ -1,19 +1,23 @@
 package me.shawshark.pizzaspleef;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import ru.tehkode.permissions.commands.CommandsManager;
 
 public class main extends JavaPlugin {
 
 	Boolean status;
+	public String Prefix = ChatColor.DARK_GRAY + "[" + ChatColor.YELLOW + "PizzaSpleef" + ChatColor.DARK_GRAY + "]";
 	
 	public void onEnable() {
 		try {
 			PluginManager pm = Bukkit.getPluginManager();
 			status = true;
 			
-			pm.registerEvents(new Command);
+			pm.registerEvents(new Commands(this), this);
 			
 		} catch(Exception ex) {
 			ex.printStackTrace();
