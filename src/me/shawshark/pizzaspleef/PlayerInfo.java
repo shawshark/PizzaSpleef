fc1d6089733643ae059d8ca4f8dd3a59d95d2d10
Chris <chrissbolding@gmail.com>
2014-04-03 20:08:19 +1100

    Create PlayerInfo.java

---
+++ src/me/shawshark/pizzaspleef/PlayerInfo.java
@@ -0,0 +1,14 @@
+package me.shawshark.pizzaspleef;
+
+public class PlayerInfo {
+	
+	String playerName;
+	String arenaName;
+	int arenaID;
+	
+	public PlayerInfo(String playerName, String arenaName, int arenaID) {
+		this.playerName = playerName;
+		this.arenaName = arenaName;
+		this.arenaID = arenaID;
+	}
+}
